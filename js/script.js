ec95a463150d2f7e56d61bdb3098b035af159883
Kabelo Martin Mayisa <kabelomayisa@outlook.com>
2019-04-22 16:02:46 +0200

    Final Changes and Ready for Submission

--- js/script.js
+++ js/script.js
@@ -1,43 +1,23 @@
 /******************************************
 Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
+project 1 - A Random Quote Generator 
 ******************************************/
 
 //Array of objects that include the quotes
 const quotes = 
 [
-  //quote #0
-  {
-    quote: 'Life is about making an impact, not making an income.',
-    source: 'Kevin Kruse',
-    citation: 'twitter'
-  },
   //quote #1
-  {
-    quote: 'Whatever the mind of man can conceive and believe, it can achieve.',
-    source: 'Napoleon Hill',
-    citation: 'brainyquotes.com'
-
-  },
+  {quote: 'A goal without a plan is just a wish.',source: 'Antoine de Saint-Exupéry, writer and pioneering aviator',citation: 'Jeff Rich Quotes. (n.d.).',year: 1995},
   //quote #2
-  {
-    quote: 'Strive not to be a success, but rather to be of value.',
-    source: 'Albert Einstein',
-    citation: 'brainyquotes.com'
-
-  },
+  {quote: 'By failing to prepare, you are preparing to fail.',source: 'Benjamin Franklin, Founding Father of the United States',citation: 'Benjamin Franklin Quotes. (n.d.).',year: 1919 ,tags: '#Fail, #Failing, #Prepare, #Preparing, #You'},
   //quote #3
-  {
-    quote: 'Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.',
-    source: 'Robert Frost',
-    citation: 'brainyquotes.com'
-  },
+  {quote: 'If you don’t know where you are going, you’ll end up someplace else.',source: 'Yogi Berra, former New York Yankees catcher',citation: 'Yogi Berra Quotes. (n.d.).', tags: '#Because, #Careful, #Get, #Going, #Got, #Know, #Might, #Very, #Where, #You'},
   //quote #4
-  {
-    quote: 'I attribute my success to this: I never gave or took any excuse.',
-    source: 'Florence Nightingale',
-    citation: 'brainyquotes.com'
-  }
+  {quote: 'Unless commitment is made, there are only promises and hopes; but no plans.',source: 'Peter F. Drucker, author and educator',citation: 'Peter Drucker Quotes. (n.d.).'},
+  //quote #5
+  {quote: 'Plans are of little importance, but planning is essential.',source: 'Winston Churchill, former British Prime Minister',citation: 'Winston Churchill Quotes. (n.d.).',year: 1941},
+  //quote #6
+  {quote: 'I am prepared for the worst, but hope for the best.',source: 'Benjamin Disraeli, British politician and author', citation: 'Benjamin Disraeli Quotes. (n.d.).'}
 ];
 
 
@@ -49,74 +29,54 @@ let getRandomQuote = () =>
   return quotes[randomNumber];
 } 
 
+//this is just a function that returns a random color which will be set as the background color
+let getRandomColor = () =>{
+  const bgcolors = [
+    '#cb4bca',
+    '#ff8d00',
+    '#010649',
+    '#008d23',
+    '#f45042'
+  ];
 
+  let randNum = Math.floor(Math.random() * 5);
+  return bgcolors[randNum];
+}
 
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
+//the function that will print the Random quote we get from the getRandomQuote function 
 let printQuote = () => 
 {
   let randomQuote = getRandomQuote();
-
-
-
-  /*
-
-THIS IS WHAT THE INSTRUCTIONS REQUIRE US TO DO
-
   let html = '';
+  let randomColor = getRandomColor();
 
-  html += '<p class = "quote">' + randomQuote.quote;
-  html += '<p class = "source">' + randomQuote.source;
-  html += '<p class = "tag"> #' + randomQuote.tag ;
+  html += '<p class = "quote">' + randomQuote.quote + '</p>'; //filling it into the class = quote p tag 
+  html += '<p class = "source">' + randomQuote.source ; //filling it into the class = source p tag 
+ 
 
-  if(randomQuote.citation){
+  if(randomQuote.citation){//checking if the quote's citation is there, and if so display it 
     html += '<span class = "citation">' + randomQuote.citation + '</span>';
+  }
+  if(randomQuote.year){ //checking if the quote's date is there, and if so display it 
+    html += '<span class = "year">' + randomQuote.year + '</span>';
+  }
+  if(randomQuote.tags){ //checking if the quote's tags are there, and if so display it
+    html += '<br /><span><strong>Tags : '+ randomQuote.tags +'</strong></span>';
   }
 
   html += '</p>';
-  
 
 
+  //when button gets clicked and shows another quote, this will also change the backround randomly
+  document.body.style.background = randomColor;
+  //change the button's color randomly
+  document.getElementById('loadQuote').style.backgroundColor = randomColor;
+
+  //setting the innerHTML of the quote-box to my html string
   document.getElementById("quote-box").innerHTML = html;
-  */
-
-//INSTEAD THIS IS WHAT I MEANT 
-
-  let quote = randomQuote.quote;
-  let source = randomQuote.source;
-  let citation = randomQuote.citation;
-
-  document.getElementById("quote").innerHTML = quote;
-  document.getElementById("source").innerHTML = source;
-  /*
-  this below line of code gives me an error which i also need help with : Uncaught TypeError: Cannot set property 'innerHTML' of null
-  at HTMLButtonElement.printQuote 
-    */
-  document.getElementById("citation").innerHTML = citation;
-
-
-
 }
 
+// displaying the objects in the html element through the printQuote function
+document.getElementById('loadQuote').addEventListener("click", printQuote, false); 
 
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function.
-***/
-
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
   
